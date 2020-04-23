0b6069eb0e4a51a7d41cd6feae45c11879a32ec4
Abdirashiid Jama <rashiid_jacfar@hotmail.com>
2020-04-23 16:32:39 +0100

    Added Application Submission Test

--- DemoWebApp.Tests/LoanApplicationTests.cs
+++ DemoWebApp.Tests/LoanApplicationTests.cs
@@ -7,6 +7,7 @@ namespace DemoWebApp.Tests
     public class LoanApplicationTests
     {
         private const string HomeUrl = "http://localhost:40077/";
+        private const string StartLoanUrl = "http://localhost:40077/Home/StartLoanApplication";
 
         [Fact]
         [Trait("Category","Smoke")]
@@ -19,6 +20,26 @@ namespace DemoWebApp.Tests
                 driver.Navigate().GoToUrl(HomeUrl);
 
                 driver.FindElement(By.Id("startApplication")).Click();
+
+            }
+        }
+
+        [Fact]
+        public void SubmitApplication()
+        {
+            using (IWebDriver driver = new FirefoxDriver())
+            {
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl(StartLoanUrl);
+
+                driver.FindElement(By.Id("FirstName")).SendKeys("Rashiid");
+                driver.FindElement(By.Id("LastName")).SendKeys("Jama");
+                driver.FindElement(By.Id("Loan")).Click();
+                driver.FindElement(By.Id("TermsAcceptance")).Click();
+                driver.FindElement(By.CssSelector(".btn.btn-primary")).Click();
+
+
 
             }
         }
